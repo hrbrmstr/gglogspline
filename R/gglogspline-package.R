69b78dd266cafdef4768bd3f8855199f94d56c8e
hrbrmstr <bob@rud.is>
2019-06-17 16:07:57 -0400

    R package repo initialization complete

---
+++ R/gglogspline-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/gglogspline>
+#' - BugReports: <https://gitlab.com/hrbrmstr/gglogspline/issues>
+#' 
+#' @md
+#' @name gglogspline
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+"_PACKAGE"
